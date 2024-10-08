cbeb09d0d87e4f63197c9d3ad2ae12c661208aab
Saran <saran152004s@gmail.com>
2024-10-08 19:09:45 +0530

    Revert "ok"
    This reverts commit a0bfb6f656284c1838295a404b824b01738d97cb.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-  // Toggle menu icon and header on click
-  $('#menu').click(function() {
+  $('#menu').click(function(){
     $(this).toggleClass('fa-times');
     $('header').toggleClass('toggle');
   });
 
-  // Remove classes and show/hide 'top' button on scroll or load
-  $(window).on('scroll load', function() {
+  $(window).on('scroll load',function(){
+
     $('#menu').removeClass('fa-times');
     $('header').removeClass('toggle');
 
-    if ($(window).scrollTop() > 0) {
-      $('.top').fadeIn(); // Use fadeIn for smooth appearance
-    } else {
-      $('.top').fadeOut(); // Use fadeOut for smooth disappearance
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
     }
+
   });
 
-  // Smooth scrolling for anchor links
-  $('a[href*="#"]').on('click', function(e) {
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
     e.preventDefault();
 
     $('html, body').animate({
-      scrollTop: $($(this).attr('href')).offset().top
-    }, 500, 'linear');
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
   });
 
 });
